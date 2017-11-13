c5b570fc436a9e424da95a253086786586259534
Donatas Pranckenas <donatas@oberlo.com>
2017-11-13 13:17:24 +0200

    resolve paths

--- dist/bundle.js
+++ dist/bundle.js
@@ -69,13 +69,125 @@
 
 "use strict";
 Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__src_sass_styles_sass__ = __webpack_require__(1);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__src_sass_styles_sass___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_0__src_sass_styles_sass__);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__src_js_header__ = __webpack_require__(1);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__src_js_footer__ = __webpack_require__(2);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__src_js_footer___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_1__src_js_footer__);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__src_sass_styles_sass__ = __webpack_require__(3);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__src_sass_styles_sass___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_2__src_sass_styles_sass__);
 
+
+
+
+
+/* On page load */
+document.addEventListener('DOMContentLoaded', function () {
+  Object(__WEBPACK_IMPORTED_MODULE_0__src_js_header__["a" /* default */])();
+});
 
 
 /***/ }),
 /* 1 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+function initNavbarBurger() {
+  const $navbarBurgers = document.querySelectorAll('.navbar-burger');
+
+  if ($navbarBurgers.length > 0) {
+
+    $navbarBurgers.forEach(($el) => {
+      $el.addEventListener('click', () => {
+        // Get the target from the "data-target" attribute
+        const target = $el.dataset.target;
+        const $target = document.getElementById(target);
+
+        // Toggle the class on both the "navbar-burger" and the "navbar-menu"
+        $el.classList.toggle('is-active');
+        $target.classList.toggle('is-active');
+      });
+    });
+  }
+}
+
+function initNavbarDropdown() {
+  const $navbarDropdown = document.querySelectorAll('.navbar-item.has-dropdown');
+
+  if ($navbarDropdown.length > 0) {
+
+    $navbarDropdown.forEach(($el) => {
+      $el.addEventListener('click', () => {
+        $el.classList.toggle('show-dropdown');
+      });
+    });
+  }
+}
+
+function initShowAllArticles() {
+  const $navbarDropdown = document.querySelector('#show-all-articles');
+  const $allArticles = document.querySelector('#all-articles');
+
+  if ($navbarDropdown && $allArticles) {
+    $navbarDropdown.addEventListener('click', () => {
+      $allArticles.classList.toggle('is-hidden-touch');
+    });
+  }
+}
+
+function getCurrentScroll() {
+  return window.pageYOffset || document.documentElement.scrollTop;
+}
+
+function initShrinkHeader() {
+  const shrinkHeader = 94;
+  const $header = document.querySelector('.navbar-primary');
+
+  if ($header) {
+    window.addEventListener('scroll', function(e) {
+      const scroll = getCurrentScroll();
+
+      $header.classList.toggle('expanded', scroll < shrinkHeader);
+    });
+  }
+}
+
+function initSearchNavbarSecondary() {
+  const $btn = document.querySelector('.nav-search-button');
+  const $navbar = document.querySelector('.navbar-secondary .navbar');
+
+  if ($btn && $navbar) {
+    $btn.addEventListener('click', () => {
+      $navbar.classList.toggle('search');
+    });
+  }
+}
+
+function initHeader() {
+  initNavbarBurger();
+  initNavbarDropdown();
+  initShowAllArticles();
+  initShrinkHeader();
+  initSearchNavbarSecondary();
+}
+
+/* harmony default export */ __webpack_exports__["a"] = (initHeader);
+
+
+/***/ }),
+/* 2 */
+/***/ (function(module, exports) {
+
+window.onload = function() {
+  const menuTitles = document.querySelectorAll('a.footer-menu-title');
+
+  menuTitles.forEach(title => title.addEventListener('click', (event) => {
+    event.preventDefault();
+    title.parentElement.classList.toggle('is-active');
+  }));
+}
+
+
+/***/ }),
+/* 3 */
 /***/ (function(module, exports) {
 
 // removed by extract-text-webpack-plugin
