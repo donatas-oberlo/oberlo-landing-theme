05ac45c91292b184b17b5d8ae69e5743dfffa727
Donatas Pranckenas <donatas@oberlo.com>
2017-11-15 18:18:02 +0200

    fixes

--- src/js/footer.js
+++ src/js/footer.js
@@ -2,6 +2,10 @@ window.onload = function() {
   const menuTitles = document.querySelectorAll('a.footer-menu-title');
 
   menuTitles.forEach(title => title.addEventListener('click', (event) => {
+    const activeElement = document.querySelector('.footer-menu.is-active');
+    if (activeElement) {
+      activeElement.classList.remove('is-active');
+    }
     event.preventDefault();
     title.parentElement.classList.toggle('is-active');
   }));
